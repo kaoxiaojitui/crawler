3053c054ca647875370e687f1657c989fceac9ae
kaoxiaojitui <kaoxiaojitui@github.com>
2018-05-08 11:38:57 +0800

    Put the image as the content
    Image is shown up in the content now.

--- oneforeveryday.py
+++ oneforeveryday.py
@@ -9,19 +9,20 @@ Created on Sat Apr 21 14:03:26 2018
 from bs4 import BeautifulSoup
 import requests
 import smtplib
-from email import encoders
+#from email import encoders
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
+#from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 import os
+from email.mime.image import MIMEImage
 
-sender = r'xxx@qq.com'
-password = 'xxx' #设置SMTP时获得的16位字符码
+sender = r'****@qq.com'
+password = '**************' #设置SMTP时获得的16位字符码
 mail_host = 'smtp.qq.com'
 receivers = ['xxx1@qq.com', 'xxx2@qq.com', 'xxx3@qq.com']
 headers = {r'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36'}
 oneUrl = r'http://wufazhuce.com/#carousel-one'
-path = '/home/yituadmin/Personal_Eric/oneforeveryday/oneImage'
+path = r'/home/yituadmin/personal_eric/oneImage'
 emailTitle = 'One [一个] -- 每天只为你准备一张图片、一篇文字和一个问答'
 
 def getHTML(url):
@@ -68,21 +69,32 @@ def sending_email(kanhao, emailContent, localImagePath):
     
     #发送方信息
     msg['From'] = sender
-    msg_content = '刊号' + kanhao + '\n' + emailContent
-    msg.attach(MIMEText(msg_content, 'plain', 'utf-8'))
+    msg_content = """
+    <p>刊号 """+ kanhao + """</p>
+    <p>""" +  emailContent +  """</p>
+    <p><img src="cid:image1"></p>    
+    """
+    
+    msg.attach(MIMEText(msg_content, 'html', 'utf-8'))
     with open(localImagePath, 'rb') as f:
+        
         # 设置附件的MIME和文件名，这里是jpg类型,可以换png或其他类型:
-        mime = MIMEBase('image', 'png', filename='oneImage.png')
+#        mime = MIMEBase('image', 'png', filename='oneImage.png')
         # 加上必要的头信息:
-        mime.add_header('Content-Disposition', 'attachment', filename='oneImage.png')
-        mime.add_header('Content-ID', '<0>')
-        mime.add_header('X-Attachment-Id', '0')
+#        mime.add_header('Content-Disposition', 'attachment', filename='oneImage.png')
+#        mime.add_header('Content-ID', '<image1>')
+#        mime.add_header('X-Attachment-Id', '0')
         # 把附件的内容读进来:
-        mime.set_payload(f.read())
+#        mime.set_payload(msgImage)
         # 用Base64编码:
-        encoders.encode_base64(mime)
+#        encoders.encode_base64(mime)
         # 添加到MIMEMultipart:
-        msg.attach(mime)
+#        msg.attach(mime)
+        
+        msgImage = MIMEImage(f.read())
+        msgImage.add_header('Content-ID', '<image1>')
+        msg.attach(msgImage)
+        
     #登录并发送邮件
     try:
         #QQsmtp服务器的端口号为465或587
